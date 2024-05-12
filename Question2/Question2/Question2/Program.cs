88208d8162d3f0a5c1931001c513523773f63627
Thai_Duy_Roan <128533558+tdr8012@users.noreply.github.com>
2024-05-12 13:57:05 -0400

    Add files via upload

---
+++ Question2/Question2/Question2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 12, 35, 10, 250, 12, -77, 35, 7, 9, 10 };
+            int[] array2 = { 12, -5, 250, 17, 12, -5, 250, 35, 10 };
+
+
+            Func<int[], int> minNumInArray = (array) =>
+            {
+                int maxNum = array.Min();
+                return Array.IndexOf(array, maxNum);
+            };
+
+            int o = minNumInArray(array1);
+            int j = minNumInArray(array2);
+
+            Console.WriteLine($"The index of the smallest element in array1: {o}");
+            Console.WriteLine();
+            Console.WriteLine($"The index of the smallest element in array2: {j}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
